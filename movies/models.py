4fd9a728feec3453fabd8e12eabece813915955a
Egejuru Chukwudinma <egejuruchukwudinma@gmail.com>
2025-08-31 16:05:28 +0100

    features(movies): model/viewset for movies with search, filter, ordering

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    release_year = models.PositiveIntegerField(null=True, blank=True)
+    genre = models.CharField(max_length=100, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created_at"]
+        indexes = [models.Index(fields=["title"])]
+
+    def __str__(self):
+        return self.title
+
 # Create your models here.
--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from rest_framework.filters import SearchFilter, OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Movie
+from .serializers import MovieSerializer
+
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_fields = ["genre", "release_year"]
+    search_fields = ["title", "genre"]
+    ordering_fields = ["release_year", "created_at", "title"]
 
 # Create your views here.
