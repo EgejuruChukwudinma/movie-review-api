f579120bc810095a189b1a6a7fbf83327de748b7
Egejuru Chukwudinma <egejuruchukwudinma@gmail.com>
2025-08-31 15:58:37 +0100

    features(accounts): custom user, register/login (JWT), profile endpoints

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from rest_framework.response import Response
+from .models import User
+from .serializers import RegisterSerializer, UserSerializer, ProfileUpdateSerializer
+
+class RegisterView(generics.CreateAPIView):
+    serializer_class = RegisterSerializer
+    permission_classes = [permissions.AllowAny]
+
+class ProfileView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = ProfileUpdateSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
+
+class MeView(generics.RetrieveAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
 
 # Create your views here.
